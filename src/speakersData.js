17b2036417fc7cf454f83cbb17f58cccc286a547
sammykirigha <dkirigha18@gmail.com>
2022-06-19 15:25:05 +0300

    created a demo app to display speakers with the best react practices

---
+++ src/speakersData.js
@@ -0,0 +1,398 @@
+import image1 from './images/01.jpg';
+import image2 from './images/02.jpg';
+import image3 from './images/03.jpg';
+import image4 from './images/04.jpg';
+
+export const data = [
+	{
+		id: "1266",
+		first: "sammy",
+		last: "kirigha",
+		image: image1,
+		company: "the jitu",
+		bio: "sammy kirigha is a junior software developer and a database manager",
+		twitterHandle: "@sammy",
+		favorite: true,
+		sessions: [
+			{
+				id: '32',
+				title: 'Rails powered by GlassFish',
+				eventYEar: '2015',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '33',
+				title: 'Learning data science',
+				eventYEar: '2014',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '34',
+				title: 'Excell with SQL',
+				eventYEar: '2013',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '35',
+				title: 'React and Angular',
+				eventYEar: '2009',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '36',
+				title: 'Nodejs with Express',
+				eventYEar: '2012',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '37',
+				title: 'Styling React components',
+				eventYEar: '2018',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '39',
+				title: 'Python data scripting',
+				eventYEar: '2007',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '40',
+				title: 'data structures and algorithms',
+				eventYEar: '2010',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '41',
+				title: 'state management',
+				eventYEar: '2018',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '42',
+				title: 'redux state vs context',
+				eventYEar: '2019',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+		]
+	},
+		{
+		id: "1268",
+		first: "jane",
+		last: "Doe",
+		image: image2,
+		company: "Google",
+		bio: "Jane Doe is a junior software developer and a database manager",
+		twitterHandle: "@jane",
+		favorite: true,
+		sessions: [
+			{
+				id: '32',
+				title: 'Rails powered by GlassFish',
+				eventYEar: '2015',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '33',
+				title: 'Learning data science',
+				eventYEar: '2014',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '34',
+				title: 'Excell with SQL',
+				eventYEar: '2013',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '35',
+				title: 'React and Angular',
+				eventYEar: '2009',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '36',
+				title: 'Nodejs with Express',
+				eventYEar: '2012',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '37',
+				title: 'Styling React components',
+				eventYEar: '2018',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '39',
+				title: 'Python data scripting',
+				eventYEar: '2007',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '40',
+				title: 'data structures and algorithms',
+				eventYEar: '2010',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '41',
+				title: 'state management',
+				eventYEar: '2018',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '42',
+				title: 'redux state vs context',
+				eventYEar: '2019',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+		]
+	},
+			{
+		id: "1269",
+		first: "Mark",
+		last: "Donald",
+		image: image3,
+		company: "Amazon",
+		bio: "Mark Donal is a junior software developer and a database manager",
+		twitterHandle: "@mark",
+		favorite: false,
+		sessions: [
+			{
+				id: '32',
+				title: 'Rails powered by GlassFish',
+				eventYEar: '2015',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '33',
+				title: 'Learning data science',
+				eventYEar: '2014',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '34',
+				title: 'Excell with SQL',
+				eventYEar: '2013',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '35',
+				title: 'React and Angular',
+				eventYEar: '2009',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '36',
+				title: 'Nodejs with Express',
+				eventYEar: '2012',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '37',
+				title: 'Styling React components',
+				eventYEar: '2018',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '39',
+				title: 'Python data scripting',
+				eventYEar: '2007',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '40',
+				title: 'data structures and algorithms',
+				eventYEar: '2010',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '41',
+				title: 'state management',
+				eventYEar: '2018',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '42',
+				title: 'redux state vs context',
+				eventYEar: '2019',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+		]
+	},
+				{
+		id: "1270",
+		first: "Joe",
+		last: "Doe",
+		image: image4,
+		company: "Microsoft",
+		bio: "Joe Doe is a junior software developer and a database manager",
+		twitterHandle: "@joedoe",
+		favorite: true,
+		sessions: [
+			{
+				id: '32',
+				title: 'Rails powered by GlassFish',
+				eventYEar: '2015',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '33',
+				title: 'Learning data science',
+				eventYEar: '2014',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '34',
+				title: 'Excell with SQL',
+				eventYEar: '2013',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			
+			{
+				id: '37',
+				title: 'Styling React components',
+				eventYEar: '2018',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '39',
+				title: 'Python data scripting',
+				eventYEar: '2007',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '40',
+				title: 'data structures and algorithms',
+				eventYEar: '2010',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '41',
+				title: 'state management',
+				eventYEar: '2018',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+			{
+				id: '42',
+				title: 'redux state vs context',
+				eventYEar: '2019',
+				room: {
+					name: 'Cornell Hall',
+					capacity: 0
+				}
+			},
+		]
+	},
+]
